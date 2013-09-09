6afe6e9814e328f66b4d0e8c83874ded240fa41f
Laurent <lb@MacBook-Pro-de-Laurent.local>
2013-09-09 12:12:41 +0200

    Initial commit

---
+++ ui-clickoutside/jquery.ui.dialog-clickoutside.js
@@ -0,0 +1,35 @@
+/* jQuery UI dialog clickoutside */
+
+$.widget( "ui.dialog", $.ui.dialog, {
+  options: {
+    clickOutside: false, // Determine if clicking outside the dialog shall close it
+    clickOutsideTrigger: "" // Element (id or class) that triggers the dialog opening 
+  },
+
+  open: function() {
+    var clickOutsideTriggerEl = $( this.options.clickOutsideTrigger );
+    var that = this;
+    
+    if (this.options.clickOutside){
+      // Add document wide click handler for the current dialoag namespace
+      $(document).on( "click.ui.dialogClickOutside" + that.eventNamespace, function(event){
+        if ( $(event.target).closest($(clickOutsideTriggerEl)).length == 0 && $(event.target).closest($(that.uiDialog)).length == 0){
+          that.close();
+        }
+      });
+    }
+    
+    
+    this._super(); // Invoke parent open method
+  },
+  
+  close: function() {
+    var that = this;
+    
+    // Remove document wide click handler for the current dialoag
+    $(document).off( "click.ui.dialogClickOutside" + that.eventNamespace );
+    
+    this._super(); // Invoke parent close method 
+  },  
+
+});
